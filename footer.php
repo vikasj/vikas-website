71c25446a75ceae160d39eaaca512db922378fd0
Vikas Joshi <vikas.joshi010@gmail.com>
2014-11-14 17:38:18 -0500

    update 3

--- footer.php
+++ footer.php
@@ -1,8 +1,19 @@
-<div class = "navbar navbar-inverse navbar-fixed-bottom">
+<div class = "navbar navbar-default navbar-fixed-bottom" height="100px">
 	<div class = "container">
-		<p class = "navbar-text pull-left">Site built by Vikas Joshi.</p>	
-	</div>
-</div>    
+        <p style="margin-top:10px;"class="text-center">
+            <a href="mailto:v2joshi@uwaterloo.ca" id="scale" class="btn btn-primary">Email Me</a> 
+                &nbsp;
+                  |
+                &nbsp;
+            <a href = "http://www.linkedin.com/pub/vikas-joshi/65/4a8/458"><img src = "images/linkedin.png" id="scale" alt="linkedin" height="27" width="103"></a>
+                &nbsp;
+                  |
+                &nbsp;
+            <a href = "https://github.com/vikasj"><img src="images/github.png" id="scale" alt="github" height="20" width="85"></a>
+        </p>
+
+    </div>
+</div>  
 
 <script src = "http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src = "js/bootstrap.min.js"></script> 
--- home.php
+++ home.php
@@ -41,22 +41,6 @@
                     </div>
                 </div>
 
-                <div class = "navbar navbar-default navbar-fixed-bottom" height="100px">
-                    <div class = "container">
-                        <p style="margin-top:10px;"class="text-center">
-                                 <a href="mailto:v2joshi@uwaterloo.ca" id="scale" class="btn btn-primary">Email Me</a> 
-                                 &nbsp;
-                                 |
-                                 &nbsp;
-                                 <a href = "http://www.linkedin.com/pub/vikas-joshi/65/4a8/458"><img src = "images/linkedin.png" id="scale" alt="linkedin" height="27" width="103"></a>
-                                 &nbsp;
-                                 |
-                                 &nbsp;
-                                 <a href = "https://github.com/vikasj"><img src="images/github.png" id="scale" alt="github" height="20" width="85"></a>
-                                 </p>
-
-                    </div>
-                </div>  
     <?php require("footer.php") ?>
         </body>
 </html>
