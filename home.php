caa5799ccd7c62fb68434d51de04337cdf8a6e83
Vikas Joshi <vikas.joshi010@gmail.com>
2014-12-04 19:48:59 -0500

    fix navbar bug 2

--- home.php
+++ home.php
@@ -3,20 +3,27 @@
         <body>
                 <nav class="navbar navbar-inverse navbar-fixed-top">
                         <div class="container">
+
+                            <div class="navbar-header">
                                
                                 <a href="home.php" id="scale" class="navbar-brand" >{Vikas Joshi}</a>
 
-                                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                                         <span class="sr-only">Toggle navigation</span>
                                         <span class="icon-bar"></span>
                                         <span class="icon-bar"></span>
                                         <span class="icon-bar"></span>
                                 </button>
+                            </div>
                                
-                                <div id="navbar" class="navbar-collapse collapse">
-                                        <ul class="nav navbar-nav">
-                                                <li class="active"><a href="home.php">Home</a></li>
-                                                <li><a href="resume.php">Resume</a></li>
+                                <div id="navbar" class="navbar-collapse collapse" aria-expanded="false">
+                                        <ul class="nav navbar-nav navbar-right">
+                                                <li class="active">
+                                                    <a href="home.php">Home</a>
+                                                </li>
+                                                <li>
+                                                    <a href="resume.php">Resume</a>
+                                                </li>
                                                 <!--<li><a href = "projects.php">Projects</a></li>-->
                                         </ul>
                                 </div>
